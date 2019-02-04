b53d1e0d1682866433275607d10323d384ae013c
Jaguso <jagutzo@gmail.com>
2019-02-03 21:07:59 -0600

    added two endpoints

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import
+
+import Issue from './models/Issue';
 
 const app = express();
 const router = express.Router();
@@ -15,6 +17,26 @@ const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
+});
+
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issues);
+        }
+    });
+});
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err); 
+        } else {
+            res.json(issue);
+        }
+    });
 });
 
 app.use('/', router);
