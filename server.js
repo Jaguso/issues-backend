1410ff533d84f3b4feb732acc5945d24384ee107
Jaguso <jagutzo@gmail.com>
2019-02-03 20:51:34 -0600

    finished configuration of server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World bitch'));
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+app.use('/', router);
+
 app.listen(4000, () => console.log('Express server running on port 4000'));
